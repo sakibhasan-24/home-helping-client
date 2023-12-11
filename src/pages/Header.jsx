79230e869973364a052e619ea16e949e7847a645
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-11 22:28:26 +0600

    add dynamic theme changer

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthProvider";
 import { toast } from "react-toastify";
@@ -14,12 +14,29 @@ export default function Header() {
       .catch((e) => toast.error(e.message));
   };
 
+  //
+  const [themeNumber, setThemeNumber] = useState(0);
+  const ariaLevel = ["Default", "Retro", "Cyberpunk", "Valentine", "Aqua"];
+  const value = ["default", "retro", "cyberpunk", "valentine", "aqua"];
+  const handleTheme = () => {
+    setThemeNumber((themeNumber) => themeNumber + 1);
+    if (themeNumber === 4) setThemeNumber(0);
+  };
+
   return (
     <header className=" bg-blue-950 px-2 py-6 text-white flex items-center justify-between font-semibold">
       <div>
         <Link to="">Home</Link>
       </div>
       <div className="flex items-center justify-center space-x-4 ">
+        <input
+          onClick={handleTheme}
+          type="radio"
+          name="theme-buttons"
+          className="btn theme-controller join-item"
+          aria-label={ariaLevel[themeNumber]}
+          value={value[themeNumber]}
+        />
         <Link to="">About</Link>
         <Link to="">Service</Link>
         <Link to="">Blog</Link>
