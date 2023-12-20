901363bbf04478b00f14133c5f88f4ad3f93be65
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-12-20 23:20:47 +0600

    implement pagination

--- src/Bookings/Bookings.jsx
+++ src/Bookings/Bookings.jsx
@@ -5,14 +5,47 @@ import Swal from "sweetalert2";
 export default function Bookings() {
   const { user } = useContext(AuthContext);
   const [bookings, setBookings] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  console.log(user?.email);
-  const url = `http://localhost:5000/bookings?email=${user?.email}`;
+  const [getOnlyDataCount, setGetOnlyDataCount] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [numberOfItemPerPage, setNumberOfItemPerPage] = useState(3);
+  // console.log(user?.email);
+  const url = `http://localhost:5000/bookings?email=${user?.email}&page=${currentPage}&size=${numberOfItemPerPage}`;
+  // console.log(url);
   useEffect(() => {
     fetch(url, { credentials: "include" })
       .then((res) => res.json())
-      .then((data) => setBookings(data));
+      .then((data) => {
+        setBookings(data);
+        // console.log(data);
+      });
   }, [url]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/bookings?email=${user?.email}`, {
+      credentials: "include",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setGetOnlyDataCount(data);
+        // console.log(data);
+      });
+  }, [url]);
+
+  // paginations
+  // per page 3
+  // setcurrent page
+  // find total page
+
+  const totalPage = Math.ceil(getOnlyDataCount.length / numberOfItemPerPage);
+  const pageList = [...Array(totalPage).keys()];
+  console.log("pages", pageList);
+  const handleCurrentPage = (page) => {
+    // console.log(page);
+    setCurrentPage(page);
+  };
+  const handleCurrentPageChange = (e) => {
+    setNumberOfItemPerPage(parseInt(e.target.value));
+    setCurrentPage(0);
+  };
 
   const handleDelete = (id) => {
     Swal.fire({
@@ -46,7 +79,7 @@ export default function Bookings() {
   return (
     <div className=" w-full lg:max-w-full mx-auto shadow-2xl my-8 text-center">
       <h1 className="text-4xl font-bold">
-        {user?.displayName} has {bookings.length} bookings
+        {user?.displayName} has {getOnlyDataCount.length} bookings
       </h1>
       <div>
         {bookings.map((booking) => (
@@ -56,6 +89,31 @@ export default function Bookings() {
             handleDelete={handleDelete}
           />
         ))}
+      </div>
+      {/* paginations */}
+      <div className="bg-red-400 p-4 rounded-lg">
+        {pageList.map((btn) => (
+          <button
+            style={{ backgroundColor: currentPage === btn ? "red" : "black" }}
+            onClick={() => handleCurrentPage(btn)}
+            className="bg-slate-900 text-white p-2 rounded-lg ml-4"
+            key={btn}
+          >
+            {btn}
+          </button>
+        ))}
+        <select
+          name=""
+          id=""
+          value={numberOfItemPerPage}
+          onChange={handleCurrentPageChange}
+          className="ml-6"
+        >
+          <option value="1"> 1 </option>
+          <option value="2"> 2 </option>
+          <option value="3"> 3 </option>
+          <option value={getOnlyDataCount.length}>All </option>
+        </select>
       </div>
     </div>
   );
